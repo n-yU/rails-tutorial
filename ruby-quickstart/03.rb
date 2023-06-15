5457d0b2bcf8165e5645ba85af05dadbce726968
nyU <51310314+n-yU@users.noreply.github.com>
2023-06-15 11:50:12 +0900

    Apply rubocop

--- ruby-quickstart/03.rb
+++ ruby-quickstart/03.rb
@@ -4,15 +4,15 @@ class MegaGreeter
   attr_accessor :names
 
   # Create the object
-  def initialize(names = "World")
+  def initialize(names = 'World')
     @names = names
   end
 
   # Say hi to everybody
   def say_hi
     if @names.nil?
-      puts "..."
-    elsif @names.respond_to?("each")
+      puts '...'
+    elsif @names.respond_to?('each')
       # @names is a list of some kind, iterate!
       @names.each do |name|
         puts "Hello #{name}!"
@@ -25,30 +25,28 @@ class MegaGreeter
   # Say bye to everybody
   def say_bye
     if @names.nil?
-      puts "..."
-    elsif @names.respond_to?("join")
+      puts '...'
+    elsif @names.respond_to?('join')
       # Join the list elements with commas
-      puts "Goodbye #{@names.join(", ")}.  Come back soon!"
+      puts "Goodbye #{@names.join(', ')}.  Come back soon!"
     else
       puts "Goodbye #{@names}.  Come back soon!"
     end
   end
 end
 
-
-if __FILE__ == $0
+if __FILE__ == $PROGRAM_NAME
   mg = MegaGreeter.new
   mg.say_hi
   mg.say_bye
 
-  # Change name to be "Zeke"
-  mg.names = "Zeke"
+  # Change name to be 'Zeke'
+  mg.names = 'Zeke'
   mg.say_hi
   mg.say_bye
 
   # Change the name to an array of names
-  mg.names = ["Albert", "Brenda", "Charles",
-              "Dave", "Engelbert"]
+  mg.names = %w[Albert Brenda Charles Dave Engelbert]
   mg.say_hi
   mg.say_bye
 
